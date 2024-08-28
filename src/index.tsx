20dd5e6e977cbca1823c853dd601f51ce567892f
Anton Steblovskiy <steblovskiyanton@yandex.ru>
2024-08-28 12:46:23 +0300

    v.2

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import clsx from 'clsx';
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
 import { ArticleStateType, defaultArticleState } from './constants/articleProps';
-
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
 
